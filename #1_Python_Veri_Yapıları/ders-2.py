07b9c26b110d29f924cdbb18cbe70a464298429f
macbookpro <emrefikirlier@gmail.com>
2022-09-20 12:31:36 +0300

    "güncel notlar"

---
+++ #1_Python_Veri_Yapıları/ders-2.py
@@ -0,0 +1,34 @@
+# Matematiksel İşlemler
+
+
+
+
+
+
+# Kullanıcıdan Girdi Alamak
+
+
+
+
+
+# Örnek Uygulama
+
+# Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın.
+
+
+
+
+
+# Bir aracın kilometrede ne kadar yaktığı ve kaç kilometre yol yaptığı bilgilerini alın ve sürücünü toplam ne kadar ödemesini gerektiğini hesaplayın.
+
+
+
+
+
+# Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun.
+# Beden Kitle İndeksi : Kilo / Boy(m) Boy(m)
+
+
+
+
+
