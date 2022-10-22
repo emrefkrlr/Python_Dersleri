77730273d130c6b279aa828b002365660f626e0c
macbookpro <emrefikirlier@gmail.com>
2022-10-22 21:37:56 +0300

    "son ders notu"

--- #5_Fonksiyonlar/hesap_fonksiyonlari.py
+++ #5_Fonksiyonlar/hesap_fonksiyonlari.py
@@ -1,5 +1,8 @@
 # toplama işleme
 
+from io import UnsupportedOperation
+
+
 def toplam(sayi1, sayi2):
 
     sonuc = sayi1 + sayi2
@@ -15,15 +18,26 @@ def cikarma(sayi1, sayi2):
 
 def bölme(sayi1, sayi2):
 
-    if sayi2 != 0:
+    try:
 
         sonuc = sayi1 / sayi2
 
         return sonuc
-    
-    else:
 
-        return "{} sayısı 0 a bölünemez".format(sayi1)
+    except ZeroDivisionError:
+
+        print("Error: Sıfıra bölme hatası oldu. İkinci 0 olamaz.")
+    
+    except AttributeError:
+
+        print("Error: Attribute error")
+
+    except Exception as e:
+        print("Error: Other error. {}".format(e))
+
+    
+
+    
 
 
 def carpma(sayi1, sayi2):
@@ -35,3 +49,45 @@ def carpma(sayi1, sayi2):
 def genel_hesap(a, b):
 
     return carpma(sayi1=a, sayi2=b)
+
+
+
+
+
+def sonsuz_toplama(*args):
+
+    try:
+        
+        toplam = 0
+        for i in args:
+
+            if isinstance(i, int): # Gelen veri integer mı kontorü yapar
+
+                print("Sayı: ", i)
+
+                toplam = toplam + i
+
+        return toplam
+
+    except Exception as e:
+            
+        print("sonsuz_toplama error: {}".format(e))
+
+
+def kisi_bilgileri(**kwargs):
+
+    try:
+
+        for i, v in kwargs.items():
+
+            print("""
+                Başlık:     {}   
+                Değer:      {}
+
+            """.format(i, v))
+
+        return "Bitti"
+
+    except Exception as e:
+        print("kisi_bilgileri error: {}".format(e))
+
--- #5_Fonksiyonlar/hesap_makinesi.py
+++ #5_Fonksiyonlar/hesap_makinesi.py
@@ -1,9 +1,17 @@
-from hesap_fonksiyonlari import toplam, cikarma, bölme, carpma, genel_hesap
+from hesap_fonksiyonlari import toplam, cikarma, bölme, carpma, genel_hesap, sonsuz_toplama, kisi_bilgileri
 
-print(bölme(11, 0))
+#print(bölme(11, "ali"))
 
 
-print(genel_hesap(11, 22))
+#print(genel_hesap(11, 22))
+
+
+#print(sonsuz_toplama(22, 33, 22, 11, 12, 13, 11, 33,54,65,87,9,12,43, 11.3, 22.43, 9.21, "emre"))
+
+
+print(kisi_bilgileri(isim="emre", soyisim="fikirlier", yas=30, aktif=True, ortalama=33.11, programlama_dilleri=["Python", "PHP", "JAVA"]))
+
+
 
 
 
