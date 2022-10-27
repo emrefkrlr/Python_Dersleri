84da7db16b834618e96ae4001e84833a68899454
macbookpro <emrefikirlier@gmail.com>
2022-10-27 17:11:13 +0300

    "Güncellendi"

--- #1_Python_Veri_Yapıları/ders-1.py
+++ #1_Python_Veri_Yapıları/ders-1.py
@@ -19,167 +19,85 @@ print(manav_listesi[1])
 
 print(manav_listesi[8])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-STATUS = 1
-
-## Sayısal Değişkenler İnt Float
-
-a = 5
-#print(type(a))
-
-b = 5.1
-#print(type(b))
-
-c = "5"
-##print(type(c))
-
-
-## String İfadeler
-
-# String Indeksleme ve Parçalama
-
-name = "Emre Fikirlier"
-#print(name[0])
-#print(name[:3])
-#print(name[:3], "...")
-
-# len
-
-#print("KARAKTER SAYISINI VER")
-#print(len(name))
-
-#print("Değişken Tipi")
-#print(type(name))
-
-# format Kullanımı
-
-
-
-
-
-## Listeler
-
-# İndex Kavramı
-a = ["emre", "fikirlier", 30]
-
-
-#print(a[2])
-
-#print("KARAKTER SAYISINI VER")
-#print(len(a))
-
-liste = [ 
-    [1,2,3,4,5],
-	['emre', 'fikirlier', 2, "3", 4],
-	[]
-]
-
-#print("Değişken Tipi")
-#print(type(a))
-
-#urunler = ["Beypazari", "FASDF123_asd", 22.3, "Yeil"]
-
-#print(liste[1])
-
-#print(liste[1][2])
-
-# Değişken Değiştirmek
-
-#print(a[1])
-a[1] = "Mehmet"
-#print(a[1])
-
-
-
-
-## Tupple (Demet)
-
-# b = ("emre", "fikirlier")
-# print("KARAKTER SAYISINI VER")
-# print(len(b))
-
-# # İndex Kavramı
-
-# print("------TUPLE-------")
-
-# print(b)
-
-# print(b[0])
-
-# print("Değişken Tipi")
-# print(type(b))
-
-# Değişken Değiştirmek
-
-#b[0] = "ali"
-#print(b)
-
-
-
-## Dictionary (Sözlük)
-
-c = {
-
-    "urun_adi" : "Beypazarı Soda",
-    "fiyat": 1.25,
-    "sise_rengi": "Yeşil"
-
+liste_icinde_liste = [
+    "emre", 
+    [21, 43, 23 ,0, 11, "istanbul", True, ["turkiye", "ankara"]],
+    ["python", "php", 22],
+    "fikirlier"
+    ]
+
+# listeye erişmek
+print(liste_icinde_liste[1][7][1])
+
+# liste elemanlarını değiştirmek
+liste_icinde_liste[1][6] = 99999999999999
+print(liste_icinde_liste)
+
+# listeye aşağıdaki eleman ekleyemiyoruz
+liste_icinde_liste[4] = "Bardak"
+print(liste_icinde_liste)
+
+
+# 2 - Tupple (demet)
+manav = ("elma", 16, True, 2.1)
+
+print(manav)
+print(manav[0])
+
+# Tupple eleman değitrimeye izin vermez
+manav[0] = "armut"
+print(manav)
+
+# 3 - Sözlükler
+
+kisi_listesi = {
+    "isim": "Emre",
+    "soyisim": "Fikirlier",
+    "sehir": "Istanbul",
+    "yas": 28,
+    "status": True,
+    "oran": 12.1,
+    "programla_diller": ["python", "php"]
 }
 
-# Eleman Erişimi
+# Sözlük elemanlarına erişmek
+print(kisi_listesi["isim"])
 
-# print(c["sise_rengi"])
-# print(c["urun_adi"], c["sise_rengi"], c["fiyat"])
+print(kisi_listesi["sehir"])
 
-
-# # Elaman Ekleme
-
-# c["sise_etiketi"] = "TR12_fsA"
-# print(c)
-
-# # Eleman Değerini Değiştirme
-# c["fiyat"] = 2.23
-# print(c)
+print(kisi_listesi["programla_diller"][1])
 
 
-# Temel Sözlük Metodları
-# print(c.keys())
-# print(c.values())
-# print(c.items())
+# Sözlük de eleman değiştirme
+kisi_listesi["sehir"] = "Kayseri"
+
+print(kisi_listesi["sehir"])
+
+kisi_listesi["yas"] = "30"
+
+print(kisi_listesi["yas"])
 
 
-# print("KARAKTER SAYISINI VER")
-# print(len(c))
-
-# print("Değişken Tipi")
-# print(type(c))
-
+# Sözlük de yeni eleman eklemek
+kisi_listesi["ücret"] = 1000
+print(kisi_listesi)
+print(kisi_listesi["ücret"])
 
 
+# LEN Fonksiyonu
+
+print(len("emre fikirlier"))
+isim = "emre fikirlier"
+
+print(isim[5])
+
+
+# Type Fonksiyonu
+
+name = "Emre" 
+age = 21 
+ortalama = 4.44
+status = False
+liste = [12, 23, 43]
+
+print(type(kisi_listesi))
